46549da22cac027eda505f94f0c7a8cab7e03563
Shiqi Lu <lu.shiq@northeastern.edu>
2023-08-09 13:59:47 -0400

    update

--- timetomaster-app/src/components/ShiqiHomePage/statisticsPage.tsx
+++ timetomaster-app/src/components/ShiqiHomePage/statisticsPage.tsx
@@ -1,18 +1,17 @@
 import styles from './goalsPage.module.scss';
-import React from 'react';
+import React, { useState }  from 'react';
 import Header from './components/header/header';
 import TypeSelector from './components/typeSelector/typeSelector';
 import GoalCard from './components/goalCard/goalCard';
-const habits = [
-  { id: 1, name: '学习Java' },
-  { id: 2, name: '学习Python' },
-  { id: 3, name: '学习英语' },
-];
+
+
 
 export default function sqpage() {
+    const [selectedTab, setSelectedTab] = useState('Statistics');
+
   return (
     <div className= {styles.pageContainer}>
-      <Header/>
+      <Header selectedTab={selectedTab} />
 
       <main className={styles.mainContent}>
         <div></div>
