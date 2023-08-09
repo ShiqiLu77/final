7d4fde8f0db304e5e8dfcac75efa71f3b1a545ae
Shiqi Lu <lu.shiq@northeastern.edu>
2023-08-09 00:05:37 -0400

    update

--- timetomaster-app/src/components/ShiqiHomePage/components/header/header.tsx
+++ timetomaster-app/src/components/ShiqiHomePage/components/header/header.tsx
@@ -1,37 +1,45 @@
 import styles from './header.module.scss';
 import React from 'react';
 import { Link } from "react-router-dom";
+import images from '../../images';
+
 
 const habits = [
-  { id: 1, name: '学习Java' },
-  { id: 2, name: '学习Python' },
-  { id: 3, name: '学习英语' },
+    { id: 1, name: '学习Java' },
+    { id: 2, name: '学习Python' },
+    { id: 3, name: '学习英语' },
 ];
 
 export default function sqpage() {
-  return (
-      <header className={styles.header}>
-        <div className={styles.headerContent}>
-          <div className={styles.logo}>logo</div>
-          <div className={styles.navbar}>
-            <div className={styles.tab}>
-                <Link className = "" to="/jw">
-                    Today
-                </Link>
+    return (
+        <header className={styles.header}>
+            <div className={styles.headerContent}>
+                <div className={styles.logo}>
+                    logo
+                </div>
+
+                <div className={styles.navbar}>
+                    <div className={styles.tab}>
+                        <Link className="" to="/jw">
+                            Today
+                        </Link>
+                    </div>
+                    <div className={styles.tab}>
+                        <Link className={styles.selectedLink} to="/jw">
+                            Goals
+                        </Link>
+                    </div>
+                    <div className={styles.tab}>
+                        <Link className="" to="/jw">
+                            Achievement
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={styles.setting}>
+                    <img src="./settingicon.png" alt="" />
+                </div>
             </div>
-            <div className={styles.tab}>
-                <Link className = {styles.selectedLink} to="/jw">
-                    Goals
-                </Link>
-            </div>
-            <div className={styles.tab}>
-                <Link className = "" to="/jw">
-                    Achievement
-                </Link>
-            </div>
-          </div>
-          <div className={styles.setting}>right bar</div>
-        </div>
-      </header>
-  );
+        </header>
+    );
 }
