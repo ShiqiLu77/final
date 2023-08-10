4a6f8f074042bd68141cbfc0aef5a8a2f470088e
Jiawei Wang <wang.jiawei2@northeastern.edu>
2023-08-10 12:27:33 -0400

    update

--- timetomaster-app/src/app/page.tsx
+++ timetomaster-app/src/app/page.tsx
@@ -6,18 +6,13 @@ import GoalsPage from '../components/ShiqiHomePage/goalsPage';
 import StatisticsPage from '../components/ShiqiHomePage/statisticsPage';
 import JiaweiPage from '../components/JiaweiHomePage/jwpage'; 
 import JinlingPage from '../components/JinlingHomePage/jlpage';
-<<<<<<< HEAD
-import SubmitTimePage from '../components/submit_time_page_files/SubmitTimePage';
-import TomatoClockDiv from '../components/TomatoClock/tomatoClockpage';
-import TaskDetailPage from '../components/taskDetail/taskDetailPage';
-import CreateNewGoal from '../components/CreateNewGoal/CreateNewGoal';
-import Calendar from '../components/Calendar/Calendar';
-=======
+
 import SubmitTimePage from '../components/ShiqiHomePage/components/submit_time_page_files/SubmitTimePage';
 import TomatoClockDiv from '../components/ShiqiHomePage/components/TomatoClock/tomatoClockpage';
 import TaskDetailPage from '../components/ShiqiHomePage/components/editGoalModal/editGoalModal'
 import CreateNewGoal from '../components/CreateNewGoal/CreateNewGoal'
->>>>>>> fefc7c26a4c8cf78c5466bfd72f936385c7f2d3d
+import Calendar from '../components/Calendar/Calendar';
+
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false); // 添加状态
 
@@ -33,13 +28,9 @@ export default function Home() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-<<<<<<< HEAD
-        {/* <Route path="/" element={<StatisticsPage />} /> */}
-        <Route path="/sq" element={<ShiqiPage />} />
-=======
+
         {/* <Route path="/" element={<GoalsPage />} /> */}
         <Route path="/sq" element={<GoalsPage />} />
->>>>>>> fefc7c26a4c8cf78c5466bfd72f936385c7f2d3d
         <Route path="/jw" element={<JiaweiPage />} />
         <Route path="/jl" element={<JinlingPage />} />
         <Route path="/statistics" element={<StatisticsPage />} />
